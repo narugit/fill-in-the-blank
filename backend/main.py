40437dcf8935422fc89448e9616523c5120fa79e
narugit <narusens@gmail.com>
2020-03-02 22:50:15 +0900

    change model to cnn in backend

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,14 @@ import numpy as np
 import pandas as pd
 from tensorflow.keras.models import load_model
 
-model = load_model('model.h5', compile=True)
+model = load_model('models/cnn_model.h5', compile=True)
 data = sys.stdin.readline()
- 
+
 loaded_data = pd.json_normalize(json.loads(data))
 input_data = []
 input_data.append(loaded_data['data'][0])
-prediction_result = model.predict_classes(np.array(input_data).reshape(-1, 784))
+input_data = np.array(input_data).reshape(1, 28, 28, 1)
+
+prediction_result = model.predict_classes(input_data)
 
 print(prediction_result)
