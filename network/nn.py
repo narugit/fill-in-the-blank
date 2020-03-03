61b75ecb050eda6f55f8cdf8d8089faa06e2cf13
narugit <narusens@gmail.com>
2020-03-03 23:38:19 +0900

    add network for cnn and nn

---
+++ network/nn.py
@@ -0,0 +1,54 @@
+# データのロード
+import numpy as np
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# データの整形
+x_train = x_train.reshape(-1, 784)
+x_test = x_test.reshape(-1, 784)
+
+# ここから
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train /= 255
+x_test /= 255
+# ここまでがないと予測精度ひどい
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# モデル構築
+model = Sequential()
+
+model.add(Dense(units=256, input_shape=(784,)))
+model.add(Activation('relu'))
+model.add(Dense(units=100))
+model.add(Activation('relu'))
+model.add(Dense(units=10))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd', metrics=['accuracy'])
+
+# 学習
+model.fit(x_train, y_train, batch_size=1000, epochs=100,
+          verbose=1, validation_data=(x_test, y_test))
+
+# 性能評価
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# 予測
+input_data = []
+input_data.append(x_test[100])
+input_data = np.asarray(input_data)
+print(model.predict(input_data))
+
+# モデルの保存
+model.save('nn_model.h5', include_optimizer=True)
